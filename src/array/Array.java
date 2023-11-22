75fe2937a9a9a00cd2a94d7dac3509bc471ca50b
Goutamvante <goutamvant@gmail.com>
2023-11-22 17:12:52 +0530

    removeAt method added

--- src/array/Array.java
+++ src/array/Array.java
@@ -4,24 +4,37 @@ public class Array {
 
 	private int[] items;
 	private int count;
+
 	public Array(int length) {
-		items=new int[length];
+		items = new int[length];
 	}
+
 	public void inser(int item) {
-		if(items.length==count) {
-			int[] newItems=new int[count*2];
-			for(int i=0;i<count;i++) 
-				newItems[i]=items[i];
-			items=newItems;
-			
+		if (items.length == count) {
+			int[] newItems = new int[count * 2];
+			for (int i = 0; i < count; i++)
+				newItems[i] = items[i];
+			items = newItems;
+
 		}
-		items[count++]=item;
-		
+		items[count++] = item;
+
 	}
+
+	public void removeAt(int index) {
+		if (index < 0 || index >= count)
+			throw new IllegalArgumentException();
+
+		for (int i = index; i < count - 1; i++) {
+			items[i] = items[i + 1];
+		}
+		count--;
+	}
+
 	public void print() {
-		for(int i=0;i<count;i++) {
+		for (int i = 0; i < count - 1; i++) {
 			System.out.println(items[i]);
 		}
 	}
-	
+
 }
