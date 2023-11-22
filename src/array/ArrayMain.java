7f1e4e8d494ed3942b61e697fdb9581d8b2b07e5
Goutamvante <goutamvant@gmail.com>
2023-11-22 16:54:46 +0530

    Array Main class

---
+++ src/array/ArrayMain.java
@@ -0,0 +1,15 @@
+package array;
+
+public class ArrayMain {
+
+	public static void main(String[] args) {
+		Array numbers=new Array(3);
+		numbers.inser(10);
+		numbers.inser(20);
+		numbers.inser(30);
+		numbers.inser(40);
+		numbers.inser(50);
+		numbers.inser(60);
+		numbers.print();
+	}
+}
