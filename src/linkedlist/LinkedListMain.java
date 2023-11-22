00a750cdc0babe2882cb06363aa41b8f0bf09df9
Goutamvante <goutamvant@gmail.com>
2023-11-22 20:20:18 +0530

    LinkedListMain

---
+++ src/linkedlist/LinkedListMain.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		LinkedList list=new LinkedList();
+		list.addFirst(10);
+		list.addFirst(20);
+		list.addFirst(30);
+		list.addFirst(40);
+		
+		System.out.println(list);
+	}
+}
