1e6e7995c86055c74e22e1b79444e7b0cada4a97
Goutam <goutambyte@gmail.com>
2024-01-28 17:56:21 +0530

    optimised

--- src/array/Urlify.java
+++ src/array/Urlify.java
@@ -4,35 +4,35 @@ public class Urlify {
 
 	public static void main(String[] args) {
 		String input = "Mr John Smith    ";
-		int trueLength = 13; // Length of "Mr John Smith"
+		int trueLength = 13;
 		char[] charArray = input.toCharArray();
 
-		// Call the replaceSpaces method
 		Urlify.replaceSpaces(charArray, trueLength);
 
 		// Print the result
 		System.out.println("URLified string: " + new String(charArray));
 	}
 
-	public static void replaceSpaces(char[] str, int trueLength) {
-		int spaceCount = 0, index, i = 0;
-		for (i = 0; i < trueLength; i++) {
+	private static void replaceSpaces(char[] str, int trueLength) {
+		int spaceCount = 0;
+		for (int i = 0; i < trueLength; i++) {
 			if (str[i] == ' ')
 				spaceCount++;
+
 		}
-		index = trueLength + (spaceCount * 2);
+		int index = trueLength + (spaceCount * 2);
 		if (trueLength < str.length)
 			str[trueLength] = '\0';
-		for (i = trueLength - 1; i >= 0; i--) {
+		for (int i = trueLength - 1; i >= 0; i--) {
 			if (str[i] == ' ') {
 				str[index - 1] = '0';
 				str[index - 2] = '2';
 				str[index - 3] = '%';
 				index = index - 3;
 			} else {
-				str[index - 1] = str[i];
-				index--;
+				str[--index] = str[i];
 			}
 		}
 	}
+
 }
