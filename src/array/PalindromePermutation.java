fd4141c2c1c118aa03cdc615d76dea6faae76736
Goutam <goutambyte@gmail.com>
2024-01-28 20:07:54 +0530

    Optimised

--- src/array/PalindromePermutation.java
+++ src/array/PalindromePermutation.java
@@ -1,4 +1,4 @@
-package array;
+package array;import java.io.CharConversionException;
 
 public class PalindromePermutation {
 	public static void main(String[] args) {
@@ -8,7 +8,7 @@ public class PalindromePermutation {
 		System.out.println(isPermutationOfPalindrome(input1));
 		System.out.println(isPermutationOfPalindrome(input2));
 	}
-
+/*
 	public static boolean isPermutationOfPalindrome(String phrase) {
 		int[] table = buildCharFrequecyTable(phrase);
 		return checkMaxOneOdd(table);
@@ -47,6 +47,34 @@ public class PalindromePermutation {
 			}
 		}
 		return true;
+	}
+*/
+
+	private static boolean isPermutationOfPalindrome(String phrase) {
+	    int countOdd = 0;
+	    int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+	    for (char c : phrase.toCharArray()) {
+	        int x = getCharNumeric(c);
+	        if (x != -1) {
+	            table[x]++;
+	            if (table[x] % 2 == 1) {
+	                countOdd++;
+	            } else {
+	                countOdd--;
+	            }
+	        }
+	    }
+	    return countOdd <= 1;
+	}
+
+	private static int getCharNumeric(char c) {
+	    int a = Character.getNumericValue('a');
+	    int z = Character.getNumericValue('z');
+	    int val = Character.getNumericValue(c);
+	    if (a <= val && val <= z) {
+	        return val - a;
+	    }
+	    return -1;
 	}
 
 }
