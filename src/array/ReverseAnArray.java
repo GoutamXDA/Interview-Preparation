3c7a195278250b5aff8603459d094af096ff1cc8
Goutam <goutambyte@gmail.com>
2024-02-11 00:50:35 +0530

    using toString

--- src/array/ReverseAnArray.java
+++ src/array/ReverseAnArray.java
@@ -1,10 +1,14 @@
 package array;
 
+import java.util.Arrays;
+
 public class ReverseAnArray {
 
 	public static void main(String[] args) {
 		int[] array= {1,2,3,4};
+		System.out.println(Arrays.toString(array));
 		reverse(array);
+		System.out.println(Arrays.toString(array));
 	}
 	public static void reverse(int[] array) {
 		for (int i = 0; i < array.length/2; i++) {
@@ -13,8 +17,8 @@ public class ReverseAnArray {
 			array[i]=array[other];
 			array[other]=temp;
 		}
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
+		/*
+		 * for (int i = 0; i < array.length; i++) { System.out.println(array[i]); }
+		 */
 	}
 }
