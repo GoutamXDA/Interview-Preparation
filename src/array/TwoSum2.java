a216caf184a23e1bacadfe3e10527ca80c18679e
Goutam <goutambyte@gmail.com>
2024-02-14 11:05:25 +0530

    Two sum II
    using two pointer approach

--- src/array/TwoSum2.java
+++ src/array/TwoSum2.java
@@ -5,29 +5,27 @@ import java.util.Arrays;
 public class TwoSum2 {
 	public static void main(String[] args) {
 
-		int[] nums= {2,7,11,15};
-		int target=1;
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
 		System.out.println(Arrays.toString(twoSum(nums, target)));
 	}
-    public static int[] twoSum(int[] numbers, int target) {
-    
-    	int start=0;
-    	int end=numbers.length-1;
-    	
-    	// time complexity O(n)
-    	while (start<=end) {
-			int sum=numbers[start]+numbers[end];
-			
-			if(sum>target) {
-				end-=1;
-			}
-			else if(sum<target) {
-				start+=1;
-			}else {
-				// space complexity O(1) no extra space is used
-				return new int[] {start+1,end+1};
+
+	public static int[] twoSum(int[] numbers, int target) {
+
+		int aPointet = 0;
+		int bPointer = numbers.length - 1;
+// timecomplexity O(n)
+		while (aPointet <= bPointer) {
+// space complexity is O(1)			
+			int sum = numbers[aPointet] + numbers[bPointer];
+			if (sum > target) {
+				bPointer -= 1;
+			} else if (sum < target) {
+				aPointet += 1;
+			} else {
+				return new int[] { aPointet + 1, bPointer + 1 };
 			}
 		}
-    	return new int[]{start+1,end+1};
-    }
+		return new int[] { aPointet + 1, bPointer + 1 };
+	}
 }
