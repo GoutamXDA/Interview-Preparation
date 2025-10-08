690924b362c0d51dc5dd210b6243ec42c4ec6fdb
Goutam <goutambyte@gmail.com>
2025-10-08 23:30:49 +0530

    Validpalindrome using twopointer

---
+++ src/twopointers/ValidPalindrome.java
@@ -0,0 +1,24 @@
+package twopointers;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(str.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(str.charAt(right))) right--;
+
+            if (Character.toLowerCase(str.charAt(left)) != Character.toLowerCase(str.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
