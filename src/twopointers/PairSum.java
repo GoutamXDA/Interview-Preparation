cfcbc0bcb7fc3e97b4e8a5829f225b8fbd9c3d61
Goutam <goutambyte@gmail.com>
2025-10-08 23:17:09 +0530

    pairsum with target(two sum in sorted array)

---
+++ src/twopointers/PairSum.java
@@ -0,0 +1,22 @@
+package twopointers;
+
+import java.util.Arrays;
+
+public class PairSum {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 6, 9};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+
+    public static int[] twoSum(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) return new int[]{left, right};
+            else if (sum < target) left++;
+            else right--;
+        }
+        return new int[]{-1, -1};
+    }
+}
